dc6faf7d6a90293af1dd05f8453d198ee55f62c0
Marc Samba <marc.samba@etu.minesparis.psl.eu>
2023-12-12 17:02:19 +0100

    listes

--- algo.py
+++ algo.py
@@ -1,6 +1,45 @@
 #import des bibliothèques
 import sys
+import re
 
-#lecture du FASTA avec stdin
-def read_fasta_from_stdin():
-    pass
+def separate(): 
+    
+    sequence=[]
+    variable=[]
+    seq=True
+    var=False
+    code='[A-C-G-T]'
+
+
+    #lecture du FASTA avec stdin
+    for line in sys.stdin:
+        line=line.rstrip()
+        if line.startswith(";"):
+            pass
+        elif line.startswith(">"):
+            pass
+        elif re.match(code,line)!=None and seq==True :
+            sequence.append(line)
+            seq=False
+            var=True
+        elif re.match(code,line)!=None and var==True :
+            variable.append(line)
+            seq=True
+            var=False
+    print(sequence)
+    print(variable)
+
+
+separate()
+    
+        
+        
+
+
+
+
+
+
+    
+
+
