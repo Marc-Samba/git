cf22f9b982748961f7ba1f6245446e23e69860c8
Marc Samba <marc.samba@etu.minesparis.psl.eu>
2023-12-14 12:09:54 +0100

    création du module et du fichier tests

--- align/src/align/algo.py
+++ align/src/align/algo.py
@@ -31,13 +31,29 @@ print(sequence)
 print(variable)
 
 
-def fill_table(seq1,seq2):
+def compare(a,b,Indel,Mismatch,Match):
+    add=None
+    if a==b:
+        add=Match
+    elif a=='-' or b=='-':
+        add=Indel
+    else:
+        add=Mismatch
+        
+    
+def fill_table(seq1,seq2,indel,match,mismatch):
     cols=len(seq1)+1
     rows=len(seq2)+1
 
-    M= [[0 for _ in range(cols)], for _ in range (rows)]
+    M= [[0 for _ in range(cols)] for _ in range (rows)]
 
-    for i in range (1,line):
-        pass
+    for i in range (1,rows):
+        M[i][0]=mismatch*i
     for j in range(1,cols):
         pass
+        M[0][j]=mismatch*j
+
+    for i in range (1,rows):
+        for j in range (1,cols):
+            add=compare(seq2[i],seq1[j])
+            
---
+++ tests/test_function.py
@@ -0,0 +1,5 @@
+import align
+import pytest 
+
+def test_1():
+    pass
