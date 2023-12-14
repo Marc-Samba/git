6bf38ff65ecfbb5ace1c641922a88ed144a52cfa
Marc Samba <marc.samba@etu.minesparis.psl.eu>
2023-12-14 10:20:13 +0100

    création de la matrice

--- algo.py
+++ algo.py
@@ -1,45 +1,43 @@
 #import des bibliothèques
 import sys
 import re
-
-def separate(): 
-    
-    sequence=[]
-    variable=[]
-    seq=True
-    var=False
-    code='[A-C-G-T]'
-
-
-    #lecture du FASTA avec stdin
-    for line in sys.stdin:
-        line=line.rstrip()
-        if line.startswith(";"):
-            pass
-        elif line.startswith(">"):
-            pass
-        elif re.match(code,line)!=None and seq==True :
-            sequence.append(line)
-            seq=False
-            var=True
-        elif re.match(code,line)!=None and var==True :
-            variable.append(line)
-            seq=True
-            var=False
-    print(sequence)
-    print(variable)
-
-
-separate()
-    
-        
-        
-
-
-
-
-
+import argparse
 
     
+sequence=[]
+variable=[]
+seq=False
+var=False
+code='[A-C-G-T]+'
 
 
+#lecture du FASTA avec stdin
+for line in sys.stdin:
+    if re.match(code,line)==None:
+        if seq==True and var==True:
+            seq,var=False,False
+    else:
+        n=len(line)
+        if var==False:
+            if seq==False:
+                sequence.append(line.rstrip())
+                seq=True
+            else :
+                variable.append(line.rstrip())
+                var=True
+        else : 
+            variable[-1]=variable[-1]+line.rstrip()
+print(sequence)
+print(variable)
+
+
+def fill_table(seq1,seq2):
+    cols=len(seq1)+1
+    rows=len(seq2)+1
+
+    M= [[0 for _ in range(cols)], for _ in range (rows)]
+
+    for i in range (1,line):
+        pass
+    for j in range(1,cols):
+        pass
