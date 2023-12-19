fc1998b341ec6e2a951eea264607da615252a431
Marc Samba <marc.samba@etu.minesparis.psl.eu>
2023-12-19 10:34:31 +0100

    algo needleman-wunsch

--- align/src/align/algo.py
+++ align/src/align/algo.py
@@ -2,58 +2,68 @@
 import sys
 import re
 import argparse
+import logging
 
-    
-sequence=[]
-variable=[]
-seq=False
-var=False
-code='[A-C-G-T]+'
+logger=logging.getLogger(__name__)
 
+if __name__=="__main__":
 
-#lecture du FASTA avec stdin
-for line in sys.stdin:
-    if re.match(code,line)==None:
-        if seq==True and var==True:
-            seq,var=False,False
-    else:
-        n=len(line)
-        if var==False:
-            if seq==False:
-                sequence.append(line.rstrip())
-                seq=True
-            else :
-                variable.append(line.rstrip())
-                var=True
-        else : 
-            variable[-1]=variable[-1]+line.rstrip()
-print(sequence)
-print(variable)
+    sequence=[]
+    variable=[]
+    seq=False
+    var=False
+    code='[A-C-G-T]+'
 
 
-def compare(a,b,Indel,Mismatch,Match):
-    add=None
-    if a==b:
-        add=Match
-    elif a=='-' or b=='-':
-        add=Indel
-    else:
-        add=Mismatch
+    #lecture du FASTA avec stdin
+    for line in sys.stdin:
+        if re.match(code,line)==None:
+            if seq==True and var==True:
+                seq,var=False,False
+        else:
+            n=len(line)
+            if var==False:
+                if seq==False:
+                    sequence.append(line.rstrip())
+                    seq=True
+                else :
+                    variable.append(line.rstrip())
+                    var=True
+            else : 
+                variable[-1]=variable[-1]+line.rstrip()
+    print(sequence)
+    print(variable)
+
+
+    def compare(a,b,Indel,Mismatch,Match):
+        add=None
+        if a==b:
+            add=Match
+        elif a=='-' or b=='-':
+            add=Indel
+        else:
+            add=Mismatch
         
     
-def fill_table(seq1,seq2,indel,match,mismatch):
-    cols=len(seq1)+1
-    rows=len(seq2)+1
+    def fill_table(seq1,seq2,indel,match,mismatch):
+        cols=len(seq1)+1
+        rows=len(seq2)+1
 
-    M= [[0 for _ in range(cols)] for _ in range (rows)]
+        M= [[0 for _ in range(cols)] for _ in range (rows)]
 
-    for i in range (1,rows):
-        M[i][0]=mismatch*i
-    for j in range(1,cols):
-        pass
-        M[0][j]=mismatch*j
+        for i in range (1,rows):
+            M[i][0]=mismatch*i
+        for j in range(1,cols):
+            M[0][j]=mismatch*j
 
-    for i in range (1,rows):
-        for j in range (1,cols):
-            add=compare(seq2[i],seq1[j])
+        for i in range (1,rows):
+            for j in range (1,cols):
+                add=compare(seq2[i],seq1[j])
+                case1=seq2[i-1]+add
+                case2=seq2[i]+add
+                case3=seq1[j]+add
+                M[i][j]=min(case1,case2,case3)
             
+
+
+
