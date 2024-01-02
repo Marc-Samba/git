d8d2b2650ab45da8c2312944bf667f7eb9f3e039
Marc Samba <marc.samba@etu.minesparis.psl.eu>
2024-01-02 19:23:45 +0100

    creation of the class gameoflife

--- gameoflife.py
+++ gameoflife.py
@@ -27,63 +27,48 @@ def read_args():
 
 
 
-def create_matrice(state_file):
-    """this function returns the matrice associated to the state of the game"""
+def create_self.current_state(state_file):
+   
 
-    with open(state_file, 'r') as fichier:
-        #get the number of line
-        lignes=state_file.read_line()
-        N=len(lignes)
-        #creating the matrice
-        M=[[] for i in range (N)]
-        c=0
-        for ligne in state_file:
-            # getting rid of /n
-            read_line = ligne.strip()
-            for car in read_line :
-                M[c].append(int(car))
-            c+=1
-        return M
-
-def count_neighbors(i,j,Matrice):
-    """receives the position of a cell and the Matrice associated to the state of the game and return the number of living cells around it."""
+def count_neighbors(i,j,self.current_state):
+    """receives the position of a cell and the self.current_state associated to the state of the game and return the number of living cells around it."""
     
-    m=len(Matrice)
-    n=len(Matrice[0])
+    m=len(self.current_state)
+    n=len(self.current_state[0])
     S=0
 
     #processing the corner cells
     if i==0 and j==0: #top left cell
-        S+=Matrice[0][1]+Matrice[1][0]+Matrice[1][1]
+        S+=self.current_state[0][1]+self.current_state[1][0]+self.current_state[1][1]
     elif i==0 and j==n-1: #top right cell
-        S+=Matrice[0][n-2]+Matrice[1][n-2]+Matrice[1][n-1]
+        S+=self.current_state[0][n-2]+self.current_state[1][n-2]+self.current_state[1][n-1]
     elif i==m-1 and j==0: #bottom left cell
-        S+=Matrice[m-2][0]+Matrice[m-2][1]+Matrice[m-1][1]
+        S+=self.current_state[m-2][0]+self.current_state[m-2][1]+self.current_state[m-1][1]
     elif i==m-1 and j==n-1: #bottom right cell
-        S+=Matrice[m-1][n-2]+Matrice[m-2][n-2]+Matrice[m-2][n-1]
+        S+=self.current_state[m-1][n-2]+self.current_state[m-2][n-2]+self.current_state[m-2][n-1]
     
     #processing the side cells
     
     #cells on top
     elif i==0 and 0<j<n-1: 
-        S+=Matrice[0][j-1]+Matrice[1][j-1]+Matrice[1][j]+Matrice[1][j+1]+Matrice[0][j+1]
+        S+=self.current_state[0][j-1]+self.current_state[1][j-1]+self.current_state[1][j]+self.current_state[1][j+1]+self.current_state[0][j+1]
     
     #cells on the bottom
     elif i==m-1 and 0<j<n-1: 
-        S+=Matrice[m-1][j-1]+Matrice[m-2][j-1]+Matrice[m-2][j]+Matrice[m-2][j+1]+Matrice[m-1][j+1]
+        S+=self.current_state[m-1][j-1]+self.current_state[m-2][j-1]+self.current_state[m-2][j]+self.current_state[m-2][j+1]+self.current_state[m-1][j+1]
 
     #cells on the left side
     elif j==0 and 0<i<m-1:
-        S+=Matrice[i-1][0]+Matrice[i-1][1]+Matrice[i][1]+Matrice[i+1][1]+Matrice[i+1][0]
+        S+=self.current_state[i-1][0]+self.current_state[i-1][1]+self.current_state[i][1]+self.current_state[i+1][1]+self.current_state[i+1][0]
     
     #cells on the right side
     elif j==n-1 and 0<i<m-1:
-        S+=Matrice[i-1][n-1]+Matrice[i-1][n-2]+Matrice[i][n-2]+Matrice[i+1][n-2]+Matrice[i+1][n-1]
+        S+=self.current_state[i-1][n-1]+self.current_state[i-1][n-2]+self.current_state[i][n-2]+self.current_state[i+1][n-2]+self.current_state[i+1][n-1]
 
     #processing the rest of the cells
 
     else :
-        S+=Matrice[i-1][j-1]+Matrice[i][j-1]+Matrice[i+1][j-1]+Matrice[i+1][j]+Matrice[i+1][j+1]+Matrice[i][j+1]+Matrice[i-1][j+1]+Matrice[i-1][j]                              
+        S+=self.current_state[i-1][j-1]+self.current_state[i][j-1]+self.current_state[i+1][j-1]+self.current_state[i+1][j]+self.current_state[i+1][j+1]+self.current_state[i][j+1]+self.current_state[i-1][j+1]+self.current_state[i-1][j]                              
     
     return(S)
 
@@ -101,14 +86,14 @@ def rules(ind_ligne,ind_col,Mat):
 
 
 
-def new_mat(Matrice):
-    """receives the matrice associated to the state of the game and return the new matrice having applied the rules"""
-    m=len(Matrice)
-    n=len(Matrice[0])
-    M=copy.deepcopy(Matrice) #we copy the Matrice because we don't want to modify it. We need the same Matrice to apply the rules() fucntion every time
+def new_mat(self.current_state):
+    """receives the self.current_state associated to the state of the game and return the new self.current_state having applied the rules"""
+    m=len(self.current_state)
+    n=len(self.current_state[0])
+    M=copy.deepcopy(self.current_state) #we copy the self.current_state because we don't want to modify it. We need the same self.current_state to apply the rules() fucntion every time
     for i in range (m):
         for j in range(n):
-            M[i][j]=rules(i,j,Matrice) 
+            M[i][j]=rules(i,j,self.current_state) 
     return (M)
 
 
@@ -117,34 +102,177 @@ def empty_file(file):
         ligne = fichier.readline()
         return ligne == "" #return true if the file is empty
     
-def output_file(Matrice,o_file):
+def output_file(self.current_state,o_file):
     """reçoit la matrice correspondant au nouvelle état et modifie le fichier de sortie"""
-    m=len(Matrice)
-    n=len(Matrice[0])
+    m=len(self.current_state)
+    n=len(self.current_state[0])
 
     if empty_file(o_file):                 #the file is empty : after step 1
         with open(o_file, 'w') as fichier: #opening o_file in writing mode
             for i in range (m):
                 new_line=''                #creating a new line
                 for j in range(n):
-                    new_line=new_line+str(Matrice[i][j])  #adding all the 0 and 1 from the line on by one
+                    new_line=new_line+str(self.current_state[i][j])  #adding all the 0 and 1 from the line on by one
                 fichier.write(new_line+'\n')
 
     #the file is already filed we modify it 
     else:
         with open(o_file,'w') as fichier:          #opening o_file in writing mode
             fichier.write('')                      #on vide de fichier de l'état précédent
-            for ligne in Matrice :
+            for ligne in self.current_state :
                 new_line= ''.join(map(str, ligne)) #on convertit les éléments de la liste en str et on les concatène
                 fichier.write(new_line+'\n')       #on ajoute la nouvelle ligne au fichier de sorti
 
+
+class GameOfLife:
+    def __init__(self, input, output, d, m, f, width, height):
+        self.input = input
+        self.output = output
+        self.d = d
+        self.m = m
+        self.f = f
+        self.width = width
+        self.height = height
+        self.current_state = None
+
+    def create_matrice(self):
+        """this function returns the matrice associated to the initial state of the game"""
+        with open(self.input, 'r') as fichier:
+            #get the number of line
+            lignes=fichier.readline()
+            N=len(lignes)
+            #creating the self.current_state
+            M=[[] for i in range (N)]
+            c=0
+            for ligne in self.input:
+                # getting rid of /n
+                read_line = ligne.strip()
+                for car in read_line :
+                    M[c].append(int(car))
+                c+=1
+        return(M)
+
+
+
+    def read_initial_state(self):
+        """initialize the state of the game"""
+        self.current_state = self.create_matrice()
+
+    
+    def count_neighbors(self, i, j):
+        """receives the position of a cell and the matrice associated to the state of the game and return the number of living cells around it."""
+    
+        m=len(self.current_state)
+        n=len(self.current_state[0])
+        S=0
+
+        #processing the corner cells
+        if i==0 and j==0: #top left cell
+            S+=self.current_state[0][1]+self.current_state[1][0]+self.current_state[1][1]
+        elif i==0 and j==n-1: #top right cell
+            S+=self.current_state[0][n-2]+self.current_state[1][n-2]+self.current_state[1][n-1]
+        elif i==m-1 and j==0: #bottom left cell
+            S+=self.current_state[m-2][0]+self.current_state[m-2][1]+self.current_state[m-1][1]
+        elif i==m-1 and j==n-1: #bottom right cell
+            S+=self.current_state[m-1][n-2]+self.current_state[m-2][n-2]+self.current_state[m-2][n-1]
+        
+        #processing the side cells
+        
+        #cells on top
+        elif i==0 and 0<j<n-1: 
+            S+=self.current_state[0][j-1]+self.current_state[1][j-1]+self.current_state[1][j]+self.current_state[1][j+1]+self.current_state[0][j+1]
+        
+        #cells on the bottom
+        elif i==m-1 and 0<j<n-1: 
+            S+=self.current_state[m-1][j-1]+self.current_state[m-2][j-1]+self.current_state[m-2][j]+self.current_state[m-2][j+1]+self.current_state[m-1][j+1]
+
+        #cells on the left side
+        elif j==0 and 0<i<m-1:
+            S+=self.current_state[i-1][0]+self.current_state[i-1][1]+self.current_state[i][1]+self.current_state[i+1][1]+self.current_state[i+1][0]
+        
+        #cells on the right side
+        elif j==n-1 and 0<i<m-1:
+            S+=self.current_state[i-1][n-1]+self.current_state[i-1][n-2]+self.current_state[i][n-2]+self.current_state[i+1][n-2]+self.current_state[i+1][n-1]
+
+        #processing the rest of the cells
+
+        else :
+            S+=self.current_state[i-1][j-1]+self.current_state[i][j-1]+self.current_state[i+1][j-1]+self.current_state[i+1][j]+self.current_state[i+1][j+1]+self.current_state[i][j+1]+self.current_state[i-1][j+1]+self.current_state[i-1][j]                              
+        
+        return(S)
+
+
+    def apply_rules(self, i, j):
+        """returns the new state of a cell"""
+        if self.current_state[i][j]==1: #the cell is alive
+            if 1<self.count_neighbors(i,j,self.current_state)<4:
+                return(1) #the cell stays alive
+            else:
+                return(0) #the cell dies
+        else : # the cell is dead
+            if self.count_neighbors(i,j,self.current_state)==3:
+                return(1) #the cell becomes a living cell
+
+    def generate_next_state(self):
+        """receives the current state of the game and return the new state having applied the rules"""
+        m=len(self.current_state)
+        n=len(self.current_state[0])
+        M=copy.deepcopy(self.current_state) #we copy the matrix associated to the current state because we don't want to modify it. We need the same matrice to apply the rules every time
+        for i in range (m):
+            for j in range(n):
+                M[i][j]=self.apply_rules(self.current_state,i,j) 
+        self.current_state=M
+    
+    def empty_file(self):
+        """return true if the file is empty or false if it isn't"""
+        with open(self.output, 'r') as fichier:
+            ligne = fichier.readline()
+            return ligne == "" #return true if the output file is empty
+
+    def output_file(self):
+        """takes the current state of the game and modifies the output file"""
+        m=len(self.current_state)
+        n=len(self.current_state[0])
+
+        if self.empty_file():                 #if the file is empty : after step 1
+            with open(self.output, 'w') as fichier: #opening the output file in writing mode
+                for i in range (m):
+                    new_line=''                #creating a new line
+                    for j in range(n):
+                        new_line=new_line+str(self.current_state[i][j])  #adding all the 0 and 1 from the line on by one
+                    fichier.write(new_line+'\n')
+
+        #the file is already filed we modify it 
+        else:
+            with open(self.output,'w') as fichier:          #opening o_file in writing mode
+                fichier.write('')                      #on vide de fichier de l'état précédent
+                for ligne in self.current_state :
+                    new_line= ''.join(map(str, ligne)) #on convertit les éléments de la liste en str et on les concatène
+                    fichier.write(new_line+'\n')       #on ajoute la nouvelle ligne au fichier de sorti
+
+    def run(self):
+        self.read_initial_state()
+        step=0
+        while step<self.m: #while the number of steps is inferior to the number of steps to run
+            self.generate_next_state()
+            self.output_file()
+            step+=1
+
+def main():
+    args = read_args()
+    game = GameOfLife(args.input, args.output, args.d, args.m, args.f, args.width, args.height)
+    game.run()
+
+if __name__=="__main__":
+    main()
+
 #il reste a :
                 
-#afficher chaque étape avec pygame si d est activé 
-#run en tant que module avec main 
-#faire des classes 
-#utiliser logging
-#tester les classes et les fonctions avec pytest
-#tester la simulation avec des patterns et un certain nombre d'étape et checker si c'est bon
-#using OOP to Store data only inside private members (start with underscore character). This means no direct access to internal data.
+#afficher chaque étape avec pygame si d est activé : x
+#run en tant que module avec main : v
+#faire des classes : v
+#utiliser logging : x
+#tester les classes et les fonctions avec pytest : x
+#tester la simulation avec des patterns et un certain nombre d'étape et checker si c'est bon : x
+#using OOP to Store data only inside private members (start with underscore character). This means no direct access to internal data. : x
 
